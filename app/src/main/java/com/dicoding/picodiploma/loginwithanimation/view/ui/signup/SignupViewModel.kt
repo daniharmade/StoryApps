a019617565a3307a050d552d51566071b82873fb
Dani Harmade <12250310356@students.uin-suska.ac.id>
2024-11-17 13:57:10 +0700

    Submission 1 Accepted [4 Stars]

---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/ui/signup/SignupViewModel.kt
@@ -0,0 +1,18 @@
+package com.dicoding.picodiploma.loginwithanimation.view.ui.signup
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.picodiploma.loginwithanimation.data.repository.AuthRepository
+import com.dicoding.picodiploma.loginwithanimation.data.response.SignUpResponse
+import kotlinx.coroutines.launch
+
+class SignUpViewModel(private val repository: AuthRepository) : ViewModel() {
+    val signUpResponse: LiveData<SignUpResponse> = repository.registerResponse
+
+    fun postRegister(name: String, email: String, pass: String) {
+        viewModelScope.launch {
+            repository.register(name, email, pass)
+        }
+    }
+}
